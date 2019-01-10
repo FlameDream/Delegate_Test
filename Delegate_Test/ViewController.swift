7feec09a7c511d43a89a295738434ce327f9c52c
wangdongshen <dongshen.wang@ftimage.cn>
2019-01-10 18:41:31 +0800

    Initial Commit

---
+++ Delegate_Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Delegate_Test
+//
+//  Created by ftimage2 on 2019/1/10.
+//  Copyright © 2019 Jack Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
